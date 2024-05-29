08b8cabc34b35569fd3eca5c562642d3ea1383cf
Edwin Ng <edwinngnz+git@gmail.com>
2024-05-29 16:05:37 +0000

    generated JWT to run test_get_embeddings

--- tests/test_get_embeddings.py
+++ tests/test_get_embeddings.py
@@ -1,43 +1,31 @@
 #test_get_embeddings.py
-import pytest
-from pytest_mock import mocker
 
-'''
-@pytest.mark.asyncio
-async def test_process_data_success(async_test_client, mock_model):
-    token = "test_token"  # Assume a valid token
-    headers = {
-        "Authorization": f"Bearer {token}"
-    }
-    payload = {
-        "text": "test text"
-    }
-
-    async with async_test_client as client:  # Access the yielded client object
-        response = await client.post("/get_embeddings", json=payload, headers=headers)
-
-    assert response.status_code == 200
-    assert response.json()["message"] == "Text embeddings"
-    assert isinstance(response.json()["embeddings"], list)
-'''
+from jose import jwt
+from datetime import datetime, timedelta, timezone
+import os
+from dotenv import load_dotenv
 
 # Test function using the test_client fixture
-def test_get_embeddings_success(test_client,mock_model):
-    # Simulate a valid JWT token
-    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcxNjk3NjU2NSwianRpIjoiYmI1MjAyMWUtZDEwYy00N2U3LTk0NDYtZWVkOTZmYTdhNGRiIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IjEyMzQ1IiwibmJmIjoxNzE2OTc2NTY1LCJjc3JmIjoiNDRmNGIxN2YtYTJkZS00ZDFjLWFlMTQtMWNiZDk3ZDYwYTNjIiwiZXhwIjoxNzE2OTc3NDY1fQ.GWXDggORUxjpzQFP6IrLrr3NuSW40SlGbHEOuN8DMzo"  # Replace with a valid token structure
+def test_get_embeddings_success(test_client, mock_model):
+
+    # JWT token config
+    SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+    ALGORITHM = 'HS256'
+
+    token_data={"sub": "12345"}
+    expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    token_data.update({"exp": expire})
+    token = jwt.encode(token_data, SECRET_KEY, algorithm=ALGORITHM)
 
     # Set headers with the token
     headers = {"Authorization": f"Bearer {token}"}
 
     # Prepare test data
-    data = {"text": "This is some test text"}
-
-    # Patch model.encode within the test function
-    # mock_encode = mocker.patch.object("embedding_api.model", "encode", return_value=[0.1, 0.2, 0.3])
+    payload_data = {"text": "This is some test text"}
 
 
     # Send the POST request
-    response = test_client.post("/get_embeddings", json=data, headers=headers)
+    response = test_client.post("/get_embeddings", json=payload_data, headers=headers)
 
     # Assert the response status code
     assert response.status_code == 200
