2893b38e3fff5b8fdc2df141e0e4ee225e2519af
Joshua Ng <joshua.ng.nz@gmail.com>
2024-07-02 01:02:58 +0000

    Add test case for no text data exception
    FF-47

--- tests/test_get_embeddings.py
+++ tests/test_get_embeddings.py
@@ -6,7 +6,43 @@ import os
 from dotenv import load_dotenv
 
 # Test function using the test_client fixture
-def test_get_embeddings_success(test_client, mock_model):
+def test_get_embeddings_no_text_error(test_client):
+
+    # JWT token config
+    SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+    ALGORITHM = 'HS256'
+
+    token_data={"sub": "12345"}
+    expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    token_data.update({"exp": expire})
+    token = jwt.encode(token_data, SECRET_KEY, algorithm=ALGORITHM)
+
+    # Set headers with the token
+    headers = {"Authorization": f"Bearer {token}"}
+
+    # Prepare test data
+    payload_data = {"text": ""}
+
+
+    # Send the POST request
+    response = test_client.post("/get_embeddings", json=payload_data, headers=headers)
+    response_data = response.json()
+    print("Test Error Exception Details:")
+    print(response_data["detail"])
+
+    # Assert the response status code
+    assert response.status_code == 400
+
+    # Assert the response content (assuming expected structure)
+    response_data = response.json()
+    assert "user_id" in response_data
+    assert "embeddings" in response_data
+    assert "message" in response_data
+    assert response_data["message"] == "Text embeddings"
+
+
+# Test function using the test_client fixture
+def test_get_embeddings_success(test_client):
 
     # JWT token config
     SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
@@ -26,6 +62,8 @@ def test_get_embeddings_success(test_client, mock_model):
 
     # Send the POST request
     response = test_client.post("/get_embeddings", json=payload_data, headers=headers)
+    response_data = response.json()
+
 
     # Assert the response status code
     assert response.status_code == 200
@@ -36,5 +74,3 @@ def test_get_embeddings_success(test_client, mock_model):
     assert "embeddings" in response_data
     assert "message" in response_data
     assert response_data["message"] == "Text embeddings"
-
-    
