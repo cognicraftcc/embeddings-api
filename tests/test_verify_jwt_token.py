c0ef7e92ece7ee4b207263389ed6f1a6d57c60cb
Joshua Ng <joshua.ng.nz@gmail.com>
2024-07-16 22:37:19 +0000

    Add unit tests for verify_jwt_token method
    FF-68
    FF-69
    FF-79

---
+++ tests/test_verify_jwt_token.py
@@ -0,0 +1,59 @@
+from jose import jwt
+from datetime import datetime, timedelta, timezone
+import os
+from embedding_api import get_jwt_token, verify_jwt_token
+from fastapi import HTTPException
+import pytest
+
+# Test verify_jwt_token given expected input args
+def test_verify_jwt_token_success():
+
+    # JWT token config
+    SECRET_KEY = 'TEST_KEY'
+    ALGORITHM = 'HS256'
+
+    token_data={"sub": "12345"}
+    expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    token_data.update({"exp": expire})
+    token = jwt.encode(token_data, SECRET_KEY, algorithm=ALGORITHM)
+
+    payload = verify_jwt_token(token, SECRET_KEY)
+    
+    assert payload["sub"] == "12345"
+
+# Test verify_jwt_token given expired token
+def test_verify_jwt_token_expired_token():
+
+    # JWT token config
+    SECRET_KEY = 'TEST_KEY'
+    ALGORITHM = 'HS256'
+
+    token_data={"sub": "12345"}
+    expire = datetime.now(timezone.utc) + timedelta(minutes=-15)
+    token_data.update({"exp": expire})
+    token = jwt.encode(token_data, SECRET_KEY, algorithm=ALGORITHM)
+
+    with pytest.raises(HTTPException) as e_info:
+        verify_jwt_token(token, SECRET_KEY)
+    
+    assert e_info.value.status_code == 400
+    assert e_info.value.detail == 'Invalid token: Signature has expired.'
+
+
+# Test verify_jwt_token given a token encrypted with wrong key
+def test_verify_jwt_token_invalid_token():
+
+    # JWT token config
+    SECRET_KEY = 'TEST_KEY'
+    ALGORITHM = 'HS256'
+
+    token_data={"sub": "12345"}
+    expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    token_data.update({"exp": expire})
+    token = jwt.encode(token_data, "BAD_KEY", algorithm=ALGORITHM)
+
+    with pytest.raises(HTTPException) as e_info:
+        verify_jwt_token(token, SECRET_KEY)
+    
+    assert e_info.value.status_code == 400
+    assert e_info.value.detail == 'Invalid token: Signature verification failed.'
