566ad34c8922a1231e21e7202200a6ae2a2c1744
Joshua Ng <joshua.ng.nz@gmail.com>
2024-07-18 21:30:42 +0000

    Clean up get_embeddings integration test Remove unneeded tests

--- tests/Integration/test_api_get_embeddings.py
+++ tests/Integration/test_api_get_embeddings.py
@@ -4,81 +4,8 @@ from jose import jwt
 from datetime import datetime, timedelta, timezone
 import os
 from dotenv import load_dotenv
-import pytest
 
-# Test api call with empty string as payload value
-def test_get_embeddings_no_text_error(test_client):
-
-    # JWT token config
-    SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
-    ALGORITHM = 'HS256'
-
-    token_data={"sub": "12345"}
-    expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-    token_data.update({"exp": expire})
-    token = jwt.encode(token_data, SECRET_KEY, algorithm=ALGORITHM)
-
-    # Set headers with the token
-    headers = {"Authorization": f"Bearer {token}"}
-
-    # Prepare test data
-    payload_data = {"text": ""}
-
-
-    # Send the POST request
-    response = test_client.post("/get_embeddings", json=payload_data, headers=headers)
-    response_data = response.json()
-    print("Test Error Exception Details:")
-    print(response_data["detail"])
-
-    
-    # TODO: Remove after solving exception issue FF-65
-    if response_data["detail"] == "Unexpected error: 400: No text data received":
-        pytest.xfail("Known issue with exception handling FF-65")
-
-    # Assert the response status code
-    assert response.status_code == 400
-
-    # Assert error details
-    assert response_data["detail"] == "No text data received"
-
-# Test api call with no 'text' key in payload
-def test_get_embeddings_no_text_key_error(test_client):
-
-    # JWT token config
-    SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
-    ALGORITHM = 'HS256'
-
-    token_data={"sub": "12345"}
-    expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-    token_data.update({"exp": expire})
-    token = jwt.encode(token_data, SECRET_KEY, algorithm=ALGORITHM)
-
-    # Set headers with the token
-    headers = {"Authorization": f"Bearer {token}"}
-
-    # Prepare test data
-    payload_data = {}
-
-
-    # Send the POST request
-    response = test_client.post("/get_embeddings", json=payload_data, headers=headers)
-    response_data = response.json()
-    print("Test Error Exception Details:")
-    print(response_data["detail"])
-
-    
-    # TODO: Remove after solving exception issue FF-65
-    if response_data["detail"] == "Unexpected error: 400: No text data received":
-        pytest.xfail("Known issue with exception handling FF-65")
-
-    # Assert the response status code
-    assert response.status_code == 400
-
-    # Assert error details
-    assert response_data["detail"] == "No text data received"
-
-# Test standard api call
+# Test standard api call with real model
 def test_get_embeddings_success(test_client):
 
     # JWT token config
@@ -96,11 +23,9 @@ def test_get_embeddings_success(test_client):
     # Prepare test data
     payload_data = {"text": "This is some test text"}
 
-
     # Send the POST request
     response = test_client.post("/get_embeddings", json=payload_data, headers=headers)
     response_data = response.json()
-
 
     # Assert the response status code
     assert response.status_code == 200
